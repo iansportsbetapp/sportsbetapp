9cbe7a6e4fc680c57d8a2c2eb8e251c1453ad917
Ian Livingston <i.livingston.c@gmail.com>
2023-04-07 14:48:49 -0600

    user authentication using django default 'user' model works git push

--- config/sportsbetapp/urls.py
+++ config/sportsbetapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+
+
 ]
--- config/sportsbetapp/views.py
+++ config/sportsbetapp/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -12,3 +16,31 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def login_view(request):
+    # If the user is already logged in, redirect to the home page
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    # If this is a POST request, process the login form data
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    # Render the login template with the form
+    return render(request, 'login.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
