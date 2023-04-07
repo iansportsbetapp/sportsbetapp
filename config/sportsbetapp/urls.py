aca2dcacad6ed0abb20f4031feb516058caaa50d
Ian Livingston <i.livingston.c@gmail.com>
2023-04-07 15:07:15 -0600

    homepage toggles login/signup or logout based on user login status

--- config/sportsbetapp/urls.py
+++ config/sportsbetapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('members/', views.members, name='members'),
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register, name='register'),
-
+    path('logout/', LogoutView.as_view(), name='logout'),
 
 ]
--- config/sportsbetapp/views.py
+++ config/sportsbetapp/views.py
@@ -5,18 +5,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+##members view - THIS CAN BE DELETED
 def members(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
 
 from django.shortcuts import render
 
+##homepage view
 def home(request):
     return render(request, 'home.html')
 
+#login form
 def login_view(request):
     # If the user is already logged in, redirect to the home page
     if request.user.is_authenticated:
@@ -34,7 +38,7 @@ def login_view(request):
     # Render the login template with the form
     return render(request, 'login.html', {'form': form})
 
-
+#register/signup form
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
